f6f8ad37b73daf3b11c1f3341ce3e6a8a8bbec1e
eryo12138 <15171271104@163.com>
2021-06-02 20:58:21 +0800

    模组获取方法

---
+++ src/main/java/com/trpg/version1/mybatis/vo/ModuleListVO.java
@@ -0,0 +1,20 @@
+package com.trpg.version1.mybatis.vo;
+
+import lombok.Data;
+
+@Data
+public class ModuleListVO {
+    private Integer mid;
+    private Integer userid;
+    private String name;
+    private String descri;
+    private String type;
+    private String shorttime;
+    private String longtime;
+    private String fileurl;
+    private String ruletype;
+    private String timepublish;
+    private String timebackground;
+    private String placebackground;
+    private String recommendpeople;
+}
