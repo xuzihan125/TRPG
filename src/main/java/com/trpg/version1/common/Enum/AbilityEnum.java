f137f3bae1e75052d8c4a095dcdba064d314f7ea
xuzihan125 <xuzihan125@163.com>
2021-06-19 04:29:44 +0800

    获得

---
+++ src/main/java/com/trpg/version1/common/Enum/AbilityEnum.java
@@ -0,0 +1,38 @@
+package com.trpg.version1.common.Enum;
+
+/**
+ * @author xuzihan
+ * @version 1.0
+ * @description: TODO
+ * @data
+ **/
+public enum AbilityEnum {
+
+    CREDIT_RATING("信用评级",101),
+    CREDIT("信用评级",102);
+
+    private String name;
+    private Integer aid;
+
+    AbilityEnum(String name, Integer aid) {
+        this.name = name;
+        this.aid = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAid() {
+        return aid;
+    }
+
+    public static Integer getAid(String name){
+        for(AbilityEnum entity:AbilityEnum.values()){
+            if(entity.name.equals(name)){
+                return entity.aid;
+            }
+        }
+        return null;
+    }
+}
